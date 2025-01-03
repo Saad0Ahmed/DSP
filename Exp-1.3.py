7a95556d2e05c4d38d414c328fd0e2a228ebe420
Saad0Ahmed <robotmaxsaad@hotmail.com>
2025-01-03 15:40:10 +0530

    initial commit

---
+++ Exp-1.3.py
@@ -0,0 +1,16 @@
+#tuple
+#one item tuple
+t=(0,)
+print(t)
+
+#four item tuple
+t1=(1,2,3,4)
+print(t1)
+
+#without parenthesis
+t2=1,2,3,4
+print(t2)
+
+#nested tuple
+t3=('abc',('def,','ghi'))
+print(t3)
