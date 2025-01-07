94c641a6a795744ecfb048763c6b1338c48d593f
Saad0Ahmed <robotmaxsaad@hotmail.com>
2025-01-07 09:41:44 +0530

    Third commit

---
+++ exp-1.4.py
@@ -0,0 +1,14 @@
+#creating the empty set
+set1 = set ()
+print("Empty Set: ", set1)
+
+#adding elements to a set
+for i in range (1,6):
+    set1.add(i)
+print("Set after adding elements: ", set1)
+set2 = set ()
+
+#adding elements to set2
+for i in range (1,3):
+    set2.add(i)
+print("Set after adding elements: ", set2)
